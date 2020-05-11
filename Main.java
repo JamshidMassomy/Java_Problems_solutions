557f16edbd1b05f0f5dd6612771d9019c398855b
JamshidMassomy <jamshidmassomi@gmail.com>
2020-05-11 12:24:27 +0300

    Duplicate String

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        StringDuplicateChar stringDuplicateChar = new StringDuplicateChar();
+        stringDuplicateChar.Check("POKMSBCDQPMNBCD");
     }
 
 }
---
+++ StringDuplicateChar.java
@@ -0,0 +1,16 @@
+public class StringDuplicateChar {
+    public static void Check (String st){
+        char[] chars =st.toCharArray();
+        int counter = 0;
+        for(int x =0;x<chars.length;x++){
+            for(int y =x+1; y<chars.length;y++){
+                if(chars[x] ==chars[y]){
+                    counter++;
+                    System.out.println("duplicate char is :" +chars[x]);
+
+
+                }
+            }
+        }
+    }
+}
